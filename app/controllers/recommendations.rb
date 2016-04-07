fb942ac792571a04880077014bed40e2a849ffac
Katie Meyer <email": null>
2016-04-07 18:40:58 -0500

    Create home page and new recommendation view

--- app/controllers/recommendations.rb
+++ app/controllers/recommendations.rb
@@ -1,1 +1,18 @@
-recommendations.rb
+## READ -- see all recommendations
+get "/recommendations" do
+  @books = Book.all
+  @recommendations = Recommendation.all
+  erb :'recommendations/index'
+end
+
+## CREATE -- a new recommendation
+get "/recommendations/new" do
+  if true
+    erb :'recommendations/new'
+  else
+    halt(404, erb(:'404'))
+  end
+end
+
+post "/recommendations" do
+end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,15 @@
 class Book < ActiveRecord::Base
   has_many    :recommendations
+  # has_many    :recommendors, through: :recommendations, source: :student
 
   validates :title, :author, presence: true
+
+  # def recommendors
+  #   recommendations_array = self.recommendations
+  #   recommendors = []
+  #   recommendations_array.each do |key, value|
+  #     if key ==
+  #   end
+  # end
 
 end
